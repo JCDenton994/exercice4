c33cd780661abfb00e0c657c483f702794d5f916
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-10-07 17:48:00 -0400

    add doc

--- test/model/project_model_test.dart
+++ test/model/project_model_test.dart
@@ -3,18 +3,18 @@ import 'package:dartlero/dartlero.dart';
 import 'package:dartlero_example/dartlero_example.dart';
 
 TestDemandes(Demandes demandes){
-  
+
   group("Test demandes", () {
     setUp(() {
       var nombreDemandes = 0;
       expect(demandes.length, equals(nombreDemandes));
-       
+
     var projetfact = new Demande();
     expect(projetfact,isNotNull);
     projetfact.id = 'F123456';
     projetfact.titre = 'Réaliser le projet de facturation';
     projetfact.priorite = 'Eleve';
-    demandes.add(projetfact); 
+    demandes.add(projetfact);
   expect(demandes.length, equals(++nombreDemandes));
 
     var optimisation = new Demande();
@@ -24,7 +24,7 @@ TestDemandes(Demandes demandes){
     optimisation.priorite = 'Moyenne';
     demandes.add(optimisation);
   expect(demandes.length, equals(++nombreDemandes));
-   
+
     var ameliorations = new Demande();
     expect(ameliorations,isNotNull);
     ameliorations.id = 'A123789';
@@ -33,32 +33,32 @@ TestDemandes(Demandes demandes){
     demandes.add(ameliorations);
   expect(demandes.length, equals(++nombreDemandes));
 
-  
+
 });
-    
+
     tearDown(() {
       demandes.clear();
       expect(demandes.isEmpty, isTrue);
     });
-    
+
     test('Toutes les demandes doivent avoir un identifiant unique', () {
       expect(demandes.every((p) => p.id != null), isTrue);
     });
-    
+
     test('Sélection par id',(){
-      var idRecherche = '123456';
+      var idRecherche = 'F123456';
       var demande = demandes.find(idRecherche);
       expect(demande,isNotNull);
       expect(demande.id, equals(idRecherche));
       demande.display('Sélection par id');
-    });    
-    
+    });
+
     test('Demandes triées par id', () {
       demandes.orderByFunction((m,n) => m.compareTo(n));
       demandes.display('Demandes triées par id');
     });
-    
-    
+
+
     test ('Liste des demandes à priorité élevé', (){
       var demandeEleve = demandes.select((x) => x.Eleve);
       expect(demandeEleve.isEmpty, isFalse);
@@ -68,7 +68,7 @@ TestDemandes(Demandes demandes){
   });
 }
 
-  
+
 initDisplayModel() {
   DemandeModel demandeModel = new DemandeModel();
   demandeModel.init();
